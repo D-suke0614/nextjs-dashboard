4dd9f0ccdda26c2209fd0731d4ea32e168a91f18
Daisuke <daisuke.kida@nijibox.co.jp>
2024-03-15 18:01:14 +0900

    feat: server action

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -15,10 +15,10 @@ export default async function Page({
     page?: string;
   };
 }) {
-    const query = searchParams?.query || ''
-    const currentPage = Number(searchParams?.page) || 1
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
-    const totalPages = await fetchInvoicesPages(query)
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -29,7 +29,7 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
