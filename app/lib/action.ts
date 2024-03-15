0d136d199c18ad9faf9152c2733948fd492cfa48
Daisuke <daisuke.kida@nijibox.co.jp>
2024-03-15 18:22:04 +0900

    feat: handle error

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -26,13 +26,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  try {
+      await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
+  } catch(e) {
+    console.error(e)
+  }
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -46,18 +50,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
-  revalidatePath('dashboard/invoices');
-  redirect('/dashboard/invoices');
+  try {
+      await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+      revalidatePath('dashboard/invoices');
+      redirect('/dashboard/invoices');
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
-        DELETE FROM invoices WHERE id = ${id}
-    `;
-  revalidatePath('/dashboard/invoices');
+    throw new Error('error')
+    try {
+        await sql`
+              DELETE FROM invoices WHERE id = ${id}
+          `;
+        revalidatePath('/dashboard/invoices');
+    } catch(e) {
+        console.error(e)
+    }
 }
